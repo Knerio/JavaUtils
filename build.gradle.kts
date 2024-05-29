8c394cc10b1a89eb3011f989f0707dac2d22145f
Dario P <derioreal63@gmail.com>
2024-05-29 12:02:25 +0200

    add jackson dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.1")
     implementation("org.projectlombok:lombok:1.18.32")
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("org.jetbrains:annotations:24.1.0")
