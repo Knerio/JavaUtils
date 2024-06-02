f6f445bb07b920fdb63fedf79678605f0f16901d
Dario <derioreal63@gmail.com>
2024-06-02 20:41:11 +0200

    fix tests

--- paper/src/main/java/de/derioo/javautils/paper/test/ItemStackTest.java
+++ paper/src/main/java/de/derioo/javautils/paper/test/ItemStackTest.java
@@ -1,13 +1,12 @@
 package de.derioo.javautils.paper.test;
 
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.IOException;
 
-import static de.derioo.javautils.paper.ItemStackUtility.decodeItemStacksToBytes;
-import static de.derioo.javautils.paper.ItemStackUtility.encodeItemStacksFromBytes;
-import static org.assertj.core.api.Assertions.assertThat;
+import static de.derioo.javautils.paper.ItemStackUtility.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class ItemStackTest {
 
