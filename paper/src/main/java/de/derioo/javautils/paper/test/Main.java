2b05aa7be92e96e2841a8d69f5f59eb7a3db6e7c
Dario <derioreal63@gmail.com>
2024-06-06 21:44:02 +0200

    fix tests

--- paper/src/main/java/de/derioo/javautils/paper/test/Main.java
+++ paper/src/main/java/de/derioo/javautils/paper/test/Main.java
@@ -1,28 +1,24 @@
 package de.derioo.javautils.paper.test;
 
+import de.derioo.javautils.common.MathUtility;
 import lombok.extern.java.Log;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
-@Log
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         try {
-            long start = System.currentTimeMillis();
-            new ItemStackTest();
-            log.log(Level.FINE, "ItemstackTest passed in (" + (System.currentTimeMillis() - start) + " ms)");
-            log.log(Level.FINE, "------------");
-            log.log(Level.FINE, "All tests passed");
-            throw new RuntimeException("Test");
-//            Bukkit.shutdown();
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "Doesnt work :(");
+            getLogger().log(Level.INFO, "ItemstackTest passed in (" + (MathUtility.computeTimeTaking(ItemStackTest::new).toMillis()) + " ms)");
+            getLogger().log(Level.INFO, "------------");
+            getLogger().log(Level.INFO, "All tests passed");
+        } catch (Throwable e) {
+            getLogger().log(Level.SEVERE, "Doesnt work :(", e);
             System.exit(1);
         }
-
+        Bukkit.shutdown();
     }
 }
