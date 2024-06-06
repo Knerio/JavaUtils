6fc85f0ddc2be4b5fc7d3f6266286f5addf0b62a
Dario <derioreal63@gmail.com>
2024-06-06 21:10:40 +0200

    test to exit instead of hoping for timeout

--- paper/src/main/java/de/derioo/javautils/paper/test/Main.java
+++ paper/src/main/java/de/derioo/javautils/paper/test/Main.java
@@ -1,17 +1,28 @@
 package de.derioo.javautils.paper.test;
 
+import lombok.extern.java.Log;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
+@Log
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         try {
+            long start = System.currentTimeMillis();
             new ItemStackTest();
+            log.log(Level.FINE, "ItemstackTest passed in (" + (System.currentTimeMillis() - start) + " ms)");
+            log.log(Level.FINE, "------------");
+            log.log(Level.FINE, "All tests passed");
+            throw new RuntimeException("Test");
+//            Bukkit.shutdown();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.log(Level.SEVERE, "Doesnt work :(");
+            System.exit(1);
         }
-        Bukkit.shutdown();
+
     }
 }
