0969c755ed9b4673efadca31792fc816e9f592cf
Dario <derioreal63@gmail.com>
2024-06-07 23:37:13 +0200

    add automatic test detection

--- paper/src/main/java/de/derioo/javautils/paper/test/Main.java
+++ paper/src/main/java/de/derioo/javautils/paper/test/Main.java
@@ -1,9 +1,16 @@
 package de.derioo.javautils.paper.test;
 
 import de.derioo.javautils.common.MathUtility;
+import de.derioo.javautils.paper.test.custom.CustomTest;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
+import java.time.Duration;
 import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
@@ -11,7 +18,22 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         try {
-            getLogger().log(Level.INFO, "ItemstackTest passed in (" + (MathUtility.computeTimeTaking(() -> new ItemStackUtilityTest().testAll()).toMillis()) + " ms)");
+            Reflections reflections = new Reflections(new ConfigurationBuilder()
+                    .setUrls(ClasspathHelper.forPackage("de.derioo.javautils.paper.test"))
+                    .setScanners(new SubTypesScanner()));
+            reflections.getSubTypesOf(CustomTest.class).forEach(clazz -> {
+                try {
+                    Duration took = MathUtility.computeTimeTaking(() -> {
+                        CustomTest instance = clazz.getDeclaredConstructor().newInstance();
+                        instance.testAll();
+                    });
+                    getLogger().log(Level.INFO, clazz.getSimpleName() + ".class passed in (" + took.toMillis() + " ms)");
+                } catch (Throwable e) {
+                    System.exit(1);
+                    throw new RuntimeException(e);
+                }
+            });
+
             getLogger().log(Level.INFO, "------------");
             getLogger().log(Level.INFO, "All tests passed");
         } catch (Throwable e) {
