52c9d7458b48693ec1dfb62e9e2635544a53cbd4
Dario <derioreal63@gmail.com>
2024-06-16 00:02:44 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "de.derioo.javautils"
-    version = "2.6.13"
+    version = "2.6.14"
 
     repositories {
         mavenCentral()
